2f05f0fbce2368a8bba3c3cac59ab1d32ab48f45
Minki-Cho <minji.cho@digipen.edu>
2025-11-14 18:20:34 -0800

    add ladder code for player
    add ladder code for player

---
+++ Jumppers/Assets/Scripts/Player/ClimbController.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class WallClimbAuto : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float climbSpeed = 3f;
+    public float wallCheckDistance = 0.6f;
+    public LayerMask wallLayer;
+
+    private bool isClimbing = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        TryClimbAuto();
+    }
+
+    void FixedUpdate()
+    {
+        if (isClimbing)
+        {
+            rb.useGravity = false;
+
+            // 위로 지속적으로 이동
+            rb.velocity = new Vector3(rb.velocity.x, climbSpeed, rb.velocity.z);
+        }
+    }
+
+    void TryClimbAuto()
+    {
+        // 캐릭터 전방으로 레이케스트를 쏘기
+        if (Physics.Raycast(transform.position, transform.forward, wallCheckDistance, wallLayer))
+        {
+            StartClimb();
+        }
+        else
+        {
+            StopClimb();
+        }
+    }
+
+    void StartClimb()
+    {
+        if (!isClimbing)
+        {
+            isClimbing = true;
+            rb.useGravity = false;
+        }
+    }
+
+    void StopClimb()
+    {
+        if (isClimbing)
+        {
+            isClimbing = false;
+            rb.useGravity = true;
+        }
+    }
+}
